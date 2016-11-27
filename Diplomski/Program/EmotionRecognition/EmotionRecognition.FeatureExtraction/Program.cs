21d816e9ef2f276afe9e8c0ca747403156a46e55
Your Name <you@example.com>
2016-11-27 11:52:41 +0100

    Added Image Crop class

---
+++ Diplomski/Program/EmotionRecognition/EmotionRecognition.FeatureExtraction/Program.cs
@@ -0,0 +1,52 @@
+﻿using EmotionRecognition.Service;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionRecognition.FeatureExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FaceClassifier = new Classifier(@"C:\Users\Josip\Desktop\Emotion-recognition\Diplomski\Program\EmotionRecognition\EmotionRecognition.Common\HaarCascade\haarcascade_frontalcatface_extended.xml", 200, 1000);
+            var EyeClassifier = new Classifier(@"C:\Users\Josip\Desktop\Emotion-recognition\Diplomski\Program\EmotionRecognition\EmotionRecognition.Common\HaarCascade\haarcascade_eye.xml", 50, 800);
+            var MouthClassifier = new Classifier(@"C:\Users\Josip\Desktop\Emotion-recognition\Diplomski\Program\EmotionRecognition\EmotionRecognition.Common\HaarCascade\Mouth.xml", 50, 800);
+            var NoseClassifier = new Classifier(@"C:\Users\Josip\Desktop\Emotion-recognition\Diplomski\Program\EmotionRecognition\EmotionRecognition.Common\HaarCascade\Nose 25x15.xml", 50, 800);
+            var RightEyeClassifier = new Classifier(@"C:\Users\Josip\Desktop\Emotion-recognition\Diplomski\Program\EmotionRecognition\EmotionRecognition.Common\HaarCascade\REye.xml", 50, 800);
+            var LeftEyeClassifier = new Classifier(@"C:\Users\Josip\Desktop\Emotion-recognition\Diplomski\Program\EmotionRecognition\EmotionRecognition.Common\HaarCascade\LEye.xml", 50, 800);
+
+            var Crop = ImageCrop.GetInstance();
+
+            var imagePath = @"C:\Users\Josip\Desktop\testBase\Miro.jpg";
+            var Image = new Bitmap(imagePath);
+
+            var savePathFace = @"C:\Users\Josip\Desktop\testBase\Face1.bmp";
+            var savePathEye = @"C:\Users\Josip\Desktop\testBase\Eye1.bmp";
+            var savePathMouth = @"C:\Users\Josip\Desktop\testBase\Mouth1.bmp";
+            var savePathNose = @"C:\Users\Josip\Desktop\testBase\Nose1.bmp";
+            var savePathRightEye = @"C:\Users\Josip\Desktop\testBase\RightEye1.bmp";
+            var savePathLeftEye = @"C:\Users\Josip\Desktop\testBase\LeftEye1.bmp";
+
+
+            //Find face
+            var Face = FaceClassifier.Find(Image);
+
+            //Split face in region
+            Bitmap Nose = Crop.Crop(Face, (float)0.3, (float)0.3, (float)0.2, (float)0.2);
+
+            if(Nose != null)
+            {
+                Nose.Save(@"C:\Users\Josip\Desktop\testBase\testImage1.bmp");
+                Bitmap nose = NoseClassifier.Find(Nose);
+                if (nose != null)
+                    nose.Save(savePathNose);
+            }
+                
+
+        }
+    }
+}
---
+++ Diplomski/Program/EmotionRecognition/EmotionRecognition.Service/ImageCrop.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace EmotionRecognition.Service
+{
+    public class ImageCrop
+    {
+        private static ImageCrop Instance = null;
+
+        private ImageCrop() { }
+
+        public static ImageCrop GetInstance()
+        {
+            if (Instance != null)
+                return Instance;
+            Instance = new ImageCrop();
+            return Instance;
+        }
+
+        public Bitmap Crop(Bitmap image, float leftMargine, float rightMargine, float topMargine, float bottomMargine)
+        {
+            if (image == null)
+                return null;
+            if (leftMargine + rightMargine > 0.99)
+                return null;
+            if (topMargine + bottomMargine > 0.99)
+                return null;
+
+            //Calculate width and height
+            int width = (int)Math.Round( (1 - leftMargine + rightMargine) * image.Width );
+            int height = (int)Math.Round( (1- bottomMargine + topMargine ) * image.Height );
+
+            Rectangle destination = new Rectangle(0, 0, width, height);
+            Bitmap bmp = new Bitmap(width, height);
+
+            //Create rectangle from source image
+            Graphics g = Graphics.FromImage(bmp);
+            //Calculate X, Y coordinate, width and height
+            int xCoordinate = (int)Math.Round( leftMargine * image.Width );
+            int yCoordinate = (int)Math.Round(topMargine * image.Height);
+            int rWidth = (int)Math.Round( (1 - rightMargine - leftMargine) * image.Width );
+            int rHeight = (int)Math.Round( (1 - bottomMargine - topMargine) * image.Height );
+
+            Rectangle section = new Rectangle(xCoordinate, yCoordinate, rWidth, rHeight);
+
+            //Draw
+            g.DrawImage(image, destination, section, GraphicsUnit.Pixel);
+
+            g.Dispose();
+
+            return bmp;
+        }
+
+
+    }
+
+
+}
